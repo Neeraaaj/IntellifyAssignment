a5a992959ba214c33d016e8df47d967e36532d4a
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2023-12-24 13:24:59 +0530

    Update page.js

--- app/Signup/page.js
+++ app/Signup/page.js
@@ -33,6 +33,12 @@ const Signup = () => {
             toast.error("Passwords do not match!")
         }
     }
+    
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      console.log(window.innerWidth);
+    }
+  }, []);
   return (
     <section className="bg-white">
         <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
